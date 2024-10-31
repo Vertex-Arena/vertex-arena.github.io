48b674670f36eb038992526cc8f2d9149fcdbde7
inactivo <67324063+inactivood@users.noreply.github.com>
2024-10-31 18:37:33 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,7 +116,7 @@ $(function () {
 		var $this = $(this),
 			finalDate = $(this).data('countdown');
 		$this.countdown(finalDate, function (event) {
-			$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Days</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Hours</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
+			$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Días</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Horas</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutos</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Segundos</p></div></div>'));
 		});
 	});
 
